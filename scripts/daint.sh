90ade9e69f9bdb3e2250d3daadc594575629d8df
Maksym Planeta <mcsim.planeta@gmail.com>
2018-04-27 11:39:53 +0200

    Update daint script

--- scripts/daint.sh
+++ scripts/daint.sh
@@ -25,45 +25,54 @@ OSU_DIR=$BASE/osu-micro-benchmarks-5.4.1/mpi/collective/
 # echo cp from $DYING_LIB to $DYING_LIB_TMP
 # srun --ntasks-per-node 1 cp -f $DYING_LIB $DYING_LIB_TMP
 
+
+## Fault free case
+TYPES=Corrected
+ITERATION=1000
+MSG_SIZE="2048"
+CORR_COUNT_MAX=$MSG_SIZE
 # Extract from TASKS_PER_NODE from SLURM_TASKS_PER_NODE (e.g. "72(x4)" -> "4")
 TASKS_PER_NODE=$(echo $SLURM_TASKS_PER_NODE | sed 's/.*(\(.*\)).*/\1/g;s/[^0-9]//g')
+NNODES="$SLURM_JOB_NUM_NODES"
+REPETITION="3"
 
-# Fault free case
-ITERATION=1000
-MSG_SIZE="256"
-CORR_COUNT_MAX=$MSG_SIZE
-NNODES="{1,12,24}"
-CORR_DISTS="{0,2,4,8}"
-REPETITIONS="3"
+CORR_DISTS="{0,8}"
 FAULTS="0"
 
-COMBINATIONS=$(eval echo "$NNODES+$CORR_DISTS+$REPETITIONS+$FAULTS")
+COMBINATIONS=$(eval echo "$TYPES+$NNODES+$CORR_DISTS+$FAULTS")
 
 # Faults
-CORR_DISTS="{2,4,8}"
-REPETITIONS="3"
-FAULTS="{1,2}"
+CORR_DISTS="8"
+FAULTS="4"
 
-COMBINATIONS="$COMBINATIONS "$(eval echo "$NNODES+$CORR_DISTS+$REPETITIONS+$FAULTS")
+COMBINATIONS="$COMBINATIONS "$(eval echo "$TYPES+$NNODES+$CORR_DISTS+$FAULTS")
+
+TYPES=Native
+
+COMBINATIONS="$COMBINATIONS "$(eval echo "$TYPES+$NNODES+$CORR_DISTS+$FAULTS")
 
 cd $WORKDIR
 
 # Size         Avg Latency(us)     Min Latency(us)     Max Latency(us)  Iterations
-echo "Corr	Nnodes	Size	Avg	Min	Max	Iterations	Rep	Faults"
+echo "Type	Corr	Nnodes	Size	Avg	Min	Max	Iterations	Rep	Faults"
 
-OUTDIR=$(date "+%H-%M-%S_%d%m%y")
+OUTDIR=$(date "+%H-%M-%S_%d%m%y".$RANDOM)
 mkdir $OUTDIR
+echo $OUTDIR
 
 for NNODES in $(eval echo $NNODES)
 do
-    mpiexec hostname | sort -u | tail -n $NNODES | awk '{print $1" slots=24"}' > $OUTDIR/hostfile.$NNODES
+    srun hostname | sort -u | tail -n $NNODES > $OUTDIR/hostfile.$NNODES
 done
 
-for EXPERIMENT in $COMBINATIONS
+cat $0 > $OUTDIR/script.sh
+env > $OUTDIR/script.env
+
+for i in $(seq 1 $REPETITION)
 do
-    for i in $(seq 1 $REPETITION)
+    for EXPERIMENT in $COMBINATIONS
     do
-	read NNODES CORR_DIST REPETITION FAULT <<<$(IFS="+"; echo $EXPERIMENT)
+	read TYPE NNODES CORR_DIST FAULT <<<$(IFS="+"; echo $EXPERIMENT)
 	export NPROC=$(($TASKS_PER_NODE*$NNODES))
 	
 	OUTFILE="$OUTDIR/$EXPERIMENT+$i"
@@ -74,12 +83,20 @@ do
 	export DYING_LIST
 	export CORR_DIST
 	export CORR_COUNT_MAX
-	OUT=$(srun --export=LD_PRELOAD="$DYING_LIB" \
+	if [[ "$TYPE" == 'Corrected' ]] ; then
+	    PRELOAD_DYING="--export=LD_PRELOAD=$DYING_LIB"
+	else
+	    PRELOAD_DYING=""
+	fi
+	OUT=$(srun $PRELOAD_DYING \
+		   --cpu_bind=core \
 		   -n $NPROC \
 		   $OSU_DIR/osu_bcast -m $MSG_SIZE -f -i $ITERATION)
 	echo "$EXPERIMENT"
 	echo "$OUT"
-	echo "$CORR_DIST	$NPROC	$i	$DYING_LIST" > $OUTFILE
+	echo "$TYPE	$CORR_DIST	$NPROC	$NNODES	$i	$DYING_LIST" > $OUTFILE
 	echo "$OUT"  | tail -n +3 >> $OUTFILE
     done
 done
+
+echo $OUTDIR
