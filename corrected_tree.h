c6c7c34a255f66193a1d056ee03bceeec75e76e0
Maksym Planeta <mcsim.planeta@gmail.com>
2018-05-03 12:04:45 +0200

    Small fixes after rebase

--- corrected_tree.h
+++ corrected_tree.h
@@ -7,5 +7,3 @@ ompi_coll_base_bcast_intra_corrected(void *buff, int count,
                                      MPI_Comm comm);
 #define REPLACE_COMM_WORLD(comm) comm = replace_comm_world(comm);
 
-extern int corrected_corr_dist;
-extern int corrected_count_max;
