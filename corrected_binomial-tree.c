89edac24b2b45ef50f8cfe984420a8bd32d99467
Jan Bierbaum <jan.bierbaum@os.inf.tu-dresden.de>
2018-08-16 23:47:23 +0200

    Fix assert: leaves do not have children

--- corrected_binomial-tree.c
+++ corrected_binomial-tree.c
@@ -53,7 +53,7 @@ setup_tree_binomial(int const rank, int const comm_size,
         if ((*children)[cc] >= (unsigned const)comm_size) {
             *num_child = cc; // remember the new count (and implicitly exit loop)
             *children = realloc(*children, sizeof(size_t) * *num_child); // trim child list (optional memory optimisation)
-            assert(*children && "Out of memory while *shrinking* array");
+            assert((!*num_child || *children) && "Out of memory while *shrinking* array");
         }
     }
     return CORRT_ERR_SUCCESS;
